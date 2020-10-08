f96326bbeb08d4b0fdf8b161055686c5df6e793b
Admin <admin@BV-LM03-MacBook-Air.local>
2020-10-08 09:19:13 +0530

    Build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'source-map',
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/'),
+    path: path.join(__dirname, '/docs'),
     filename: 'index_bundle.js'
   },
   module: {
